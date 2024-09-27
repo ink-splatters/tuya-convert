56a1190f5cc1082669c05c45bcf6a20c9baa1263
Peter A. <ink.splatters@pm.me>
2024-09-27 22:59:52 +0200

    * installing python via `rye` * installing python dependencies via `uv`

--- install_prereq.sh
+++ install_prereq.sh
@@ -1,27 +1,59 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-debianInstall() {
-	sudo apt-get update
-	sudo apt-get install -y git iw dnsmasq rfkill hostapd screen curl build-essential python3-pip python3-setuptools python3-wheel python3-dev mosquitto haveged net-tools libssl-dev iproute2 iputils-ping
-	sudo python3 -m pip install --user --upgrade paho-mqtt tornado git+https://github.com/drbild/sslpsk.git pycryptodomex
+rye_install() {
+	echo Installing rye...
+	curl -sSf https://rye.astral.sh/get | RYE_INSTALL_OPTION="--yes" bash
+}
+
+rye_init() {
+	echo "Initializing rye..."
+	. "$HOME"/.rye/env
 }
 
 archInstall() {
-	sudo pacman -S --needed git iw dnsmasq hostapd screen curl python-pip python-wheel python-pycryptodomex python-paho-mqtt python-tornado mosquitto haveged net-tools openssl
-	sudo python -m pip install --user --upgrade git+https://github.com/drbild/sslpsk.git
+	sudo pacman -S --needed base-devel uv git iw dnsmasq hostapd screen curl mosquitto haveged net-tools openssl
+	echo "Dependencies from OS package manager have been installed."
+	echo
+
+	echo "This script uses python installed via rye. If rye is not installed, it will be attempted to get installed."
+	echo "This script installs python packages with uv."
+	if ! command -v rye >/dev/null 2>&1; then
+		echo "No rye in path, but do we have it installed?"
+
+		if [ -f "$HOME"/.rye/env ]; then
+			echo "Yes!"
+		else
+			echo "Definitely no."
+			rye_install
+		fi
+	fi
+
+	rye_init
+	uv venv --seed
+	source .venv/bin/activate
+
+	echo "Activated venv. Installing python dependencies..."
+	uv pip install -r requirements.txt
+
+	cat <<'EOF'
+Installed python dependencies.
+
+Your venv is ready to use.
+activate with:
+	source .venv/bin/activate
+
+EOF
 }
 
 if [[ -e /etc/os-release ]]; then
 	source /etc/os-release
 else
-	echo "/etc/os-release not found! Assuming debian-based system, but this will likely fail!"
-	ID=debian
+	echo "/etc/os-release not found!"
+	exit 1
 fi
 
-if [[ ${ID} == 'debian' ]] || [[ ${ID_LIKE-} == 'debian' ]]; then
-	debianInstall
-elif [[ ${ID} == 'arch' ]] || [[ ${ID_LIKE-} == 'arch' ]]; then
+if [[ ${ID} == 'arch' ]] || [[ ${ID_LIKE-} == 'arch' ]]; then
 	archInstall
 else
 	if [[ -n ${ID_LIKE-} ]]; then
@@ -29,8 +61,6 @@ else
 	else
 		printID="${ID}"
 	fi
-	echo "/etc/os-release found but distribution ${printID} is not explicitly supported. Assuming debian-based system, but this will likely fail!"
-	debianInstall
+	echo "/etc/os-release found but distribution ${printID} is not supported."
+	exit 1
 fi
-
-echo "Ready to start upgrade"
